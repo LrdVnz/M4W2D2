7e252070b2b00bb0236fb7a8f7641ee8a2197bb0
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-02-10 16:02:27 +0100

    Initial styling

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+ 1. Funzione di ricerca dall'input sulla pagina html.
+    - Restituisce i libri sotto forma di cards. 
+      - Ogni card ha il tasto per aggiungere al carrello.
+      - 'aggiungi al carrello' aggiunge all'oggetto carrello
+
+ 2. Funzione di carrello.
+    - Mostra la lista dei libri selezionati, prendendo dall'oggetto carrello. 
+    - Implementa un tasto per cancellare. 
+    - Calcola dinamicamente il prezzo totale ( se c'è nell'api. )
+
+Usa forEach, map, filter 
+
+*/

e77479d7e99c9e519d8b99a1d21df686a0d72dfb
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-02-10 20:48:43 +0100

    Started addToCart func

--- script.js
+++ script.js
@@ -19,47 +19,79 @@ let searchInput = document.getElementById("book-search");
 let searchBtn = document.getElementById("search-btn");
 let bookList = document.getElementById("book-list");
 
+let cartList = {
+    /* 
+    book_title : {
+        title : book title,
+        price : book price, 
+    }
+    */
+}
+
 searchBtn.addEventListener("click", (event) => {
   let searchValue = searchInput.value;
   loadBooks(searchValue);
 });
 
 let loadBooks = (searchValue) => {
-  if (searchValue == undefined) {
-    fetch(`https://striveschool-api.herokuapp.com/books`)
-      .then((response) => response.json())
-      .then((data) => {
-        bookList.innerHTML = "";
-        data.forEach((element, i) => {
-          createCard(element, i);
-        });
-      });
+  fetch(`https://striveschool-api.herokuapp.com/books`)
+    .then((response) => response.json())
+    .then((data) => {
+      bookList.innerHTML = "";
+      controlSearch(data, searchValue);
+    });
+};
+
+/* Controllo se l'utente ha inserito dell'input */
+let controlSearch = (data, searchValue) => {
+  if (searchValue === undefined) {
+    data.forEach((element, i) => {
+      createCard(element, i);
+    });
   } else {
-    /* Codice per la query. Come devo richiamare la ricerca ??? */
-/* 
-      fetch(`https://striveschool-api.herokuapp.com/books/search=${searchValue}`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      }); */
+    /*  Usa search value per controllare dentro l'array
+        Usa filter 
+    */
+
   }
 };
 
+/* Crea ogni card */
 let createCard = (element, i) => {
   bookList.innerHTML += ` 
     <div class='col-6 col-md-3'> 
      <div class="card overflow-hidden" id="card-original">
       <img src="${element.img}" class="card-img-top" alt="${element.title}">
       <div class="card-body">
-          <p class="card-text"> ${element.title} </p>
+          <p class="card-text title"> ${element.title} </p>
       </div>
     <div class="card-body d-flex justify-content-between py-1">
-      <p class="card-text ">${element.category}</a>
-      <p class="card-text ">€ ${element.price}</a>
+      <p class="card-text category">${element.category}</a>
+      <p class="card-text price">€ ${element.price}</a>
+    </div>
+    <div class="card-body">
+      <a href="#" class="btn btn-primary" onclick='addToCart(this)'>Add to cart</a>
     </div>
      </div>
     </div>
      `;
 };
 
+
+let addToCart = (this_obj) => {
+    // Dal bottone, vado a prendere la card che lo contiene, come nodo. 
+    let card = this_obj.parentNode.parentNode;
+    // Percorso per titolo : card.childNodes[3].childNodes[1].innerText
+    // Percorso per prezzo : card.childNodes[5].childNodes[2].innerText
+
+}
+
+/* Carica i libri a prescindere che l'utente abbia cercato qualcosa. */
 loadBooks();
+
+/* Aggiungi al carrello: 
+  1. Al click del bottone aggiungi il libro corrente all'oggetto carrello.
+  2. Il carrello deve avere un testo che mostra sempre la somma totale del prezzo. 
+  3. Quindi prendi il titolo del libro, e il prezzo. Saranno la sua voce nel carrello. 
+*/
+
